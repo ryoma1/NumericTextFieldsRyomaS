3c3a2e45d558419357fc82f452d7d3dd936dafb5
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-03-21 11:06:38 -0400

    added code

--- main.lua
+++ main.lua
@@ -1,7 +1,88 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: NumericTextFields
+-- Name: Ryoma Scott 
+-- Course: ICS2O/3C
+-- This program displays a math equasion and asks the user to answer
+--in a numeric textfield.
+--terminal.
 
--- Your code here
+
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 255/255, 140/255, 140/255)
+
+--create local variables
+
+local questionObject
+local correctObject
+local numericField
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+
+--local functions
+
+local function AskQuestion()
+	--generate 2 random numbers between a max. and a min. number
+	randomNumber1 = math.random(0, 10)
+	randomNumber2 = math.random(0, 10)
+
+	correctAnswer = randomNumber1 + randomNumber2
+
+
+	--create question in text object
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+end
+
+local function HideCorrect()
+	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function NumericFieldListener( event )
+
+	--User begins editing "numericField"
+	if ( event.phase == "began" ) then
+
+		--clear text field
+		event.target.text = ""
+
+		elseif event.phase == "submitted" then
+
+			--when the answer is submitted (enter key is pressed) set user input to user's answer
+			userAnswer = tonumber(event.target.text)
+
+			--if the users answer and the correct answer are the same:
+			if (userAnswer == correctAnswer) then
+				correctObject.isVisible = true
+				timer.performWithDelay(2000, HideCorrect)
+
+			end
+		end
+	end
+
+	--Object creation
+
+	--displays a question and sets the colour
+	questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+	questionObject:setTextColor(255/255, 255/255, 0/255)
+
+	--create the correct text object and make it invisible
+	correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+	correctObject:setTextColor(38/255, 90/255, 200/255)
+	correctObject.isVisible = false
+
+--create numeric field
+numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
+numericField.inputType = "number"
+
+--add the event listener for the numeric field
+numericField:addEventListener( "userInput", NumericFieldListener )
+
+--fucntion calls
+
+--call the function to ask the question
+AskQuestion()
